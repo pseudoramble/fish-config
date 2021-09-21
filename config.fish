ead1e1a61dc1c292ae6880dfc062deb6ce2b2a91
Doug Swain <pseudoramble@gmail.com>
2021-09-20 21:32:54 -0400

    Test at correct location.

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    if test -f config.local.fish && test -r config.local.fish
-        source config.local.fish
+    if test -f ~/.config/fish/config.local.fish && test -r ~/.config/fish/config.local.fish
+        source ~/.config/fish/config.local.fish
     end
 end
